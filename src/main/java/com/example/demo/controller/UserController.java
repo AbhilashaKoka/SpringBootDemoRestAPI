22c30d7621f69846c24ec5484dfb05afb91b73fb
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-10-18 16:53:15 +0530

    commit8

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 import com.example.demo.bean.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,7 @@ public class UserController {
 
 
     @PostMapping("User")
+    @ResponseStatus(HttpStatus.CREATED)
     public User CreateNewUser(@RequestBody  User user){
         System.out.println(user.getId());
         System.out.println(user.getUserName());
