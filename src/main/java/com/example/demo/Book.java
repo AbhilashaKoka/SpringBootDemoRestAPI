d5d10e00fff491886ab0de8c7ac6a92b87393547
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-10-18 14:39:24 +0530

    commit1

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private long id;
+
+    private String isbn;
+
+    private String title;
+
+    private String subTitle;
+
+    private String author;
+
+    private String published;
+
+    private String publisher;
+
+    private int pages;
+
+    private String description;
+
+    private String website;
+
+
+}
